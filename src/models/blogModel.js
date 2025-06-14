132ba7c1c61cbb4c1db5991dc4dd36eb4c33d6fa
ganidande905 <ganidande9@gmail.com>
2025-06-15 00:30:10 +0530

    added blog 1

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -3,7 +3,8 @@ export const blogs = [
     {
       title: "Why I Think the Education System Is Broken — And How I’m Growing Outside It",
       preview: "The education system promises to prepare us, but fails to deliver real-world value. Here's how I grew beyond it.",
-      link: "https://blogs.ganidande.com/portfolio-from-scratch",
+      link: "https://blog.ganidande.com/posts/why-education-is-broken/",
+      
     },
 
   ];
