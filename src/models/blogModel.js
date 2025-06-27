57577abb49690917416a77270f4fdeda10a90964
ganidande905 <ganidande9@gmail.com>
2025-06-28 02:44:48 +0530

    rearranged the blogs

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,15 +1,17 @@
 // src/models/blogmodal.js
 export const blogs = [
+
+      {
+      title: "From Monoliths to Microservices: How Software Architecture Grew Up (and Got Complicated)",
+      preview: "We started with simple monoliths, added tiers, and ended up juggling hundreds of services. Here's the journey — with all its beauty, chaos, and containers.",
+      link: "https://blog.ganidande.com/posts/the-evolution-of-application-design/",
+    },
     {
       title: "Why I Think the Education System Is Broken — And How I’m Growing Outside It",
       preview: "The education system promises to prepare us, but fails to deliver real-world value. Here's how I grew beyond it.",
       link: "https://blog.ganidande.com/posts/why-education-is-broken/",
       
     },
-    {
-      title: "From Monoliths to Microservices: How Software Architecture Grew Up (and Got Complicated)",
-      preview: "We started with simple monoliths, added tiers, and ended up juggling hundreds of services. Here's the journey — with all its beauty, chaos, and containers.",
-      link: "https://blog.ganidande.com/posts/the-evolution-of-application-design/",
-    }
+
 
   ];
